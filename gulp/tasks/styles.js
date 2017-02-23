8ca6b4e999f81c115c01d70233259c3758dd0cda
root <gerard.ramosm89@gmail.com>
2017-02-23 11:17:27 -0800

    navbar is done

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,12 +5,13 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 
     //this allows for error handling while persisting gulp watch
     .on('error', function(errorInfo){
